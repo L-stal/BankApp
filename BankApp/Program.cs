ca762e5ebd058859ca0dd8a419af012161e2429d
L-stal <stalenhag.leo@gmail.com>
2023-01-04 11:44:28 +0100

    Fixed errors and made ui more user friendly

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -1,18 +1,8 @@
-﻿using BankApp;
-using Microsoft.VisualBasic;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Reflection.Metadata;
-using System.Security.Cryptography;
-using System.Xml.Serialization;
-
-namespace BankApp
+﻿namespace BankApp
 {
     public class RunMenu
     {
-        
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generic GreedyBank");
@@ -24,7 +14,7 @@ namespace BankApp
         }
 
     }
-    public static class MainMenu 
+    public static class MainMenu
     {
         //Acount in main menu for comparing index valus, if index valus are the same run function AccountTrue
         static string[] userNames = { "Robbe", "Kjell", "Leo", "Fibbe", "Mimmi" };
@@ -43,9 +33,9 @@ namespace BankApp
 
             while (mainMenu)
             {
-                    Console.WriteLine("Please enter your user name.");
-                    Console.Write("User name:");
-                    user = Console.ReadLine();
+                Console.WriteLine("Please enter your user name.");
+                Console.Write("User name:");
+                user = Console.ReadLine();
                 // A for loops that compare user input to the names in userNames[]
                 for (int i = 0; i < userNames.Length; i++)
                 {
@@ -59,16 +49,22 @@ namespace BankApp
                         {
                             Console.Clear();
                             Console.WriteLine("Login succsesful");
-                            _accountTrue.UserIndex = i;                      
+                            _accountTrue.UserIndex = i;
                             _accountTrue.accountTrue();
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid input, please try again");
                         }
                         if (pincodes[i] != pincode)
                         {
                             tries++;
+
+                            Console.WriteLine("Invalid input, please try again");
+                            Console.Write(".");
+                            Thread.Sleep(1000);
+                            Console.Write(".");
+                            Thread.Sleep(1000);
+                            Console.Write(".");
+                            Thread.Sleep(1000);
+                            Console.Clear();
+
                         }
                         if (tries >= 3)
                         {
@@ -86,153 +82,165 @@ namespace BankApp
                             MainLogin();
                         }
                     }
-
-                }
-            }
-        }
-    }
-
-    public class Account
-    { 
-        
-
-        protected int userIndex;
-
-        public int UserIndex
-        {
-            get { return userIndex; }
-            set { userIndex = value; }
-        }
-      
-       public void accountTrue()
-       {
-            var _assets = new assets();
-            _assets.AssetsIndex = userIndex;
-
-        bool accountMenu = true;
-            while (accountMenu)
-            {
-            Console.WriteLine("What would you like to do ?\n");
-            Console.WriteLine("[1]: Look over your accounts and funds. ");
-            Console.WriteLine("[2]: Deposit funds between accounts. ");
-            Console.WriteLine("[3]: Withdraw funds.");
-            Console.WriteLine("[4]: Log out.");
-            string command = Console.ReadLine();
-
-
-            switch (command)
-            {
-                case "1":
-                        //make function prints out accoouts and funds
-                        Console.Clear();
-                        printAcc();
-                        Console.Clear();
-                        break;
-                case "2":
-                        //functction 
-                        Console.Clear();
-                        exchange();
-                        Console.Clear();
-                    break;
-                case "3":
-                        //Make function to withdaraw funds
-                        Console.Clear();
-                        withdraw();
-                        Console.Clear();
-                        break;
-                case "4":
-                        //logging out
-                        Console.Clear();
-                        Console.WriteLine("You are logging out.");
-                        Console.WriteLine("Press Any key to continue");
-                        Console.Read();
-                        Console.Clear();
-                        accountMenu = false;
-                    break;
-                default:
-                        // If the user inputs a wrong number print this
-                        Console.WriteLine("Invalid input,press [Enter] to countinue");
-                        Console.ReadLine();
-                        Console.Clear();
-                    break;
-            }
-        }
-       }
-        public void printAcc()
-        {
-            int count = 0;
-            for (int i = 0; i < assets.accounts[userIndex].Length; i++)
-            {
-                Console.Write("\n" + "|" + i + "|" +assets.accounts[userIndex][i]);
-
-                for (int j = 0; j < 1; j++)
-                {
-                    Console.Write(assets.funds[userIndex][count++] + " Sek\n");
-                }
-            }  
-            Console.WriteLine("\nPress Enter to continue");
-            Console.ReadLine();
-        }
-        public void exchange()
-        {
-            int count = 0;
-            Console.WriteLine("Choose accout to depposit money to.\n");
-            for (int i = 0; i < assets.accounts[userIndex].Length; i++)
-            {
-                Console.Write("|" + i + "|"+assets.accounts[userIndex][i]);
-                for (int j = 0; j < 1; j++)
-                {
-                    Console.WriteLine(assets.funds[userIndex][count++] + " Sek\n");
-                }
-            }
-            count= 0;
-            try
-            {
-                int choice1 = int.Parse(Console.ReadLine());
-                Console.WriteLine("Choose accout to exchange money from.");
-                for (int i = 0; i < assets.accounts[userIndex].Length; i++)
-                {
-
-                    if (choice1 == i)
-                    {
-                        count++;
-                    }
                     else
                     {
-                        Console.Write("|" + i + "|" + assets.accounts[userIndex][i]);
-                        for (int j = 0; j < 1; j++)
-                        {
-                            Console.WriteLine(assets.funds[userIndex][count++] + " Sek\n");
-                        }
+                        Console.WriteLine("Invalid input, please try again");
+                        Console.Write(".");
+                        Thread.Sleep(1000);
+                        Console.Write(".");
+                        Thread.Sleep(1000);
+                        Console.Write(".");
+                        Thread.Sleep(1000);
+                        Console.Clear();
+                        break;
+
                     }
                 }
-                int choice2 = int.Parse(Console.ReadLine());
-                Console.Clear();
-                Console.WriteLine("How much money do you want transfer?\n");
-                Console.WriteLine("From: " + assets.accounts[userIndex][choice1] + assets.funds[userIndex][choice1] + " Sek");
-                Console.WriteLine("To: " + assets.accounts[userIndex][choice2] + assets.funds[userIndex][choice2] + " Sek\n");
-                decimal transfer = decimal.Parse(Console.ReadLine());
-                assets.funds[userIndex][choice1] -= transfer;
-                assets.funds[userIndex][choice2] += transfer;
-                Console.WriteLine("test: " + assets.accounts[userIndex][choice1] + assets.funds[userIndex][choice1] + " Sek");
-                Console.WriteLine("test new: " + assets.accounts[userIndex][choice2] + assets.funds[userIndex][choice2] + " Sek\n");
-                Console.ReadLine();
-
             }
-            catch
-            {
-                Console.WriteLine("No account found with that number,try again and choose from the list above");
-                Console.ReadLine();
-                Console.Clear() ;
-                exchange();
-
-            }
-       
         }
 
-        public void withdraw()
+        public class Account
         {
-            //FAST PÅ BAR SAVING FIX IT
+
+
+            protected int userIndex;
+
+            public int UserIndex
+            {
+                get { return userIndex; }
+                set { userIndex = value; }
+            }
+
+            public void accountTrue()
+            {
+                var _assets = new assets();
+                _assets.AssetsIndex = userIndex;
+
+                bool accountMenu = true;
+                while (accountMenu)
+                {
+                    Console.WriteLine("What would you like to do ?\n");
+                    Console.WriteLine("[1]: Look over your accounts and funds. ");
+                    Console.WriteLine("[2]: Deposit funds between accounts. ");
+                    Console.WriteLine("[3]: Withdraw funds.");
+                    Console.WriteLine("[4]: Log out.");
+                    string command = Console.ReadLine();
+
+
+                    switch (command)
+                    {
+                        case "1":
+                            //make function prints out accoouts and funds
+                            Console.Clear();
+                            printAcc();
+                            Console.Clear();
+                            break;
+                        case "2":
+                            //functction 
+                            Console.Clear();
+                            exchange();
+                            Console.Clear();
+                            break;
+                        case "3":
+                            //Make function to withdaraw funds
+                            Console.Clear();
+                            withdraw();
+                            Console.Clear();
+                            break;
+                        case "4":
+                            //logging out
+                            Console.Clear();
+                            Console.WriteLine("You are logging out.");
+                            Console.WriteLine("Press Any key to continue");
+                            Console.Read();
+                            Console.Clear();
+                            accountMenu = false;
+                            break;
+                        default:
+                            // If the user inputs a wrong number print this
+                            Console.WriteLine("Invalid input,press [Enter] to countinue");
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                    }
+                }
+            }
+            public void printAcc()
+            {
+                int count = 0;
+                for (int i = 0; i < assets.accounts[userIndex].Length; i++)
+                {
+                    Console.Write("\n" + "[" + i + "]" + assets.accounts[userIndex][i]);
+
+                    for (int j = 0; j < 1; j++)
+                    {
+                        Console.Write(assets.funds[userIndex][count++] + " Sek\n");
+                    }
+                }
+                Console.WriteLine("\nPress Enter to continue");
+                Console.ReadLine();
+            }
+            public void exchange()
+            {
+                int count = 0;
+                Console.WriteLine("Choose accout to depposit money to.\n");
+
+                for (int i = 0; i < assets.accounts[userIndex].Length; i++)
+                {
+                    Console.Write("[" + i + "]" + assets.accounts[userIndex][i] + ": ");
+                    for (int j = 0; j < 1; j++)
+                    {
+                        Console.WriteLine(assets.funds[userIndex][count++] + " Sek\n");
+                    }
+                }
+                count = 0;
+                int choice1 = int.Parse(Console.ReadLine());
+
+                if (choice1 < assets.accounts[userIndex].Length)
+                {
+                    Console.WriteLine("Choose accout to exchange money from.\n");
+                    for (int i = 0; i < assets.accounts[userIndex].Length; i++)
+                    {
+
+                        if (choice1 == i)
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            Console.Write("|" + i + "|" + assets.accounts[userIndex][i] + ": ");
+                            for (int j = 0; j < 1; j++)
+                            {
+                                Console.WriteLine(assets.funds[userIndex][count++] + " Sek\n");
+                            }
+                        }
+                    }
+                    int choice2 = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("How much money do you want transfer?\n");
+                    Console.WriteLine("From:" + assets.accounts[userIndex][choice1] + "  " + assets.funds[userIndex][choice1] + " Sek");
+                    Console.WriteLine("--------");
+                    Console.WriteLine("To:" + assets.accounts[userIndex][choice2] + " " + assets.funds[userIndex][choice2] + " Sek\n");
+                    decimal transfer = decimal.Parse(Console.ReadLine());
+                    assets.funds[userIndex][choice1] -= transfer;
+                    assets.funds[userIndex][choice2] += transfer;
+                    Console.WriteLine("You exchanges money from: " + assets.accounts[userIndex][choice1] + "\nTo: " + assets.accounts[userIndex][choice2]);
+                    Console.WriteLine("Your new balance is.");
+                    Console.WriteLine(assets.accounts[userIndex][choice1] + ": " + assets.funds[userIndex][choice1] + " Sek");
+                    Console.WriteLine(assets.accounts[userIndex][choice2] + ": " + assets.funds[userIndex][choice2] + " Sek\n");
+                    Console.WriteLine("Press [Enter] to continue");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("No account found  with that number. Please try again");
+                }
+
+            }
+
+            public void withdraw()
+            {
+                //FAST PÅ BARA SAVING FIX IT
                 int count = 0;
                 Console.WriteLine("Choose account to withdraw money from\n");
                 for (int i = 0; i < assets.accounts[userIndex].Length; i++)
@@ -244,10 +252,10 @@ namespace BankApp
                         Console.Write(assets.funds[userIndex][count++] + " Sek\n");
                     }
                 }
-            try
-            {
-                int choice1;
-                choice1 = int.Parse(Console.ReadLine());
+                try
+                {
+                    int choice1;
+                    choice1 = int.Parse(Console.ReadLine());
                     Console.WriteLine("How much money do you want to withdraw?");
                     decimal withdrawM = decimal.Parse(Console.ReadLine());
                     //Fixa så man inte kan skriva tillexemple 1001 och välja konto
@@ -266,41 +274,42 @@ namespace BankApp
                         withdraw();
 
                     }
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input, choose from the list above.");
+                    Console.ReadLine();
+                    Console.Clear();
+                    withdraw();
+                }
             }
-            catch 
+
+        }
+
+        public class assets
+        {
+            protected int assetsIndex;
+            public int AssetsIndex
             {
-                Console.WriteLine("Invalid input, choose from the list above.");
-                Console.ReadLine();
-                Console.Clear();
-                withdraw();
+                get { return assetsIndex; }
+                set { assetsIndex = value; }
             }
-        }
-
-    }
-
-    public class assets
-    {
-        protected int assetsIndex;
-        public int AssetsIndex
-        {
-            get { return assetsIndex; }
-            set { assetsIndex = value; }
-        }
-        public static string[][] accounts =
-        {
-            new string[] {"Savings: ", "Salary: "},
-            new string[] {"Savings: ", "Salary: ","Z-tv Salary: "},
-            new string[] {"Savings: ", "Salary: ","Computer Parts: ","Goods: "},
-            new string[] {"Savings: ", "Salary: ","Cash for snacks: ","Vet Savings: ","Toy Account: "},
-            new string[] {"Savings: ", "Salary: ", "Knitting Salary: ","Cosplay funds: ","Fibbes Xmas present: ","Gaming Gear: "},
+            public static string[][] accounts =
+            {
+            new string[] {"Savings", "Salary"},
+            new string[] {"Savings", "Salary","Z-tv Salary"},
+            new string[] {"Savings", "Salary","Computer Parts","Goods"},
+            new string[] {"Savings", "Salary","Cash for snacks","Vet Savings","Toy Account"},
+            new string[] {"Savings", "Salary", "Knitting Salary","Cosplay funds","Fibbes Xmas present","Gaming Gear"},
         };
-        public static decimal[][] funds =
-        {
+            public static decimal[][] funds =
+            {
             new decimal[] {1000,20000}
-            , new decimal[] {1000,20004,12354}
-            , new decimal[] {1000,12312,41231,123451}
-            , new decimal[] {1000,12394,12349,123094,129394}
-            , new decimal[] {1000,12394,12341,123412,451234,124512}
+            , new decimal[] {1000,20000,12345}
+            , new decimal[] {1000,20000,12345,12341}
+            , new decimal[] {1000,12394,12349,123494,129394}
+            , new decimal[] {1000,12394,12341,12412,451234,12512}
         };
+        }
     }
 }
